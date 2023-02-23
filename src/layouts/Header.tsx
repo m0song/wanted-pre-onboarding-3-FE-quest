0a14c921b2ae59df3e2a23d1cce8509429c09a32
m0song <treaturselfwell@kakao.com>
2023-02-23 18:36:05 +0900

    Feat: make layout template

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <Link to='/'>Wanted Pre-onboarding course</Link>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/layouts/Sidebar.tsx
@@ -0,0 +1,21 @@
+// make sidebar component
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>
+        <Link to='/page-a'>Page A</Link>
+      </div>
+      <div>
+        <Link to={`page-b`}>Page B</Link>
+      </div>
+      <div>
+        <Link to='/page-c'>Page C</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
